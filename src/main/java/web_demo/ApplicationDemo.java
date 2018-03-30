779b7155505b1836be7a73e4ea473d2e981cd05d
hobn <hub@disanbo.com>
2018-03-30 15:27:41 +0800

    jenkins ss

--- src/main/java/web_demo/ApplicationDemo.java
+++ src/main/java/web_demo/ApplicationDemo.java
@@ -3,5 +3,9 @@ package web_demo;
 public class ApplicationDemo {
 	 public static void main(String[] args) {
 		System.out.println("sss");
+		
+		System.out.println("jenkins start...");
+		
+		
 	}
 }
